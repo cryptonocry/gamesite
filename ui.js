c5988a2849430ea09a57c7d26cd50d684b1087b4
cryptonocry <132585712+cryptonocry@users.noreply.github.com>
2025-03-07 18:12:31 +0100

    Update ui.js

--- ui.js
+++ ui.js
@@ -7,17 +7,10 @@ function maskWallet(wallet) {
 
 export async function showRecordsOverlay(recordsTableContainer, recordsContainer, currentPlayer) {
   const records = await fetchAllParticipantsFromXano();
-  let html = "<table><tr><th>Nickname</th><th>BTC Wallet</th><th>Score</th></tr>";
-  let currentPlayerIndex = -1;
-  records.forEach((rec, index) => {
+  let html = "<table><tr><th>Wallet</th><th>Score</th></tr>";
+  records.forEach((rec) => {
     const shortWallet = maskWallet(rec.wallet || "");
-    let rowId = "";
-    if (currentPlayer && rec.nickname === currentPlayer.nickname && rec.wallet === currentPlayer.wallet) {
-      rowId = " id='currentPlayerRow'";
-      currentPlayerIndex = index;
-    }
-    html += `<tr${rowId}>
-               <td>${rec.nickname}</td>
+    html += `<tr>
                <td>${shortWallet}</td>
                <td>${rec.score}</td>
              </tr>`;
@@ -25,11 +18,4 @@ export async function showRecordsOverlay(recordsTableContainer, recordsContainer
   html += "</table>";
   recordsTableContainer.innerHTML = html;
   recordsContainer.style.display = "block";
-
-  setTimeout(() => {
-    const row = document.getElementById("currentPlayerRow");
-    if (row) {
-      row.scrollIntoView({ behavior: "smooth", block: "center" });
-    }
-  }, 100);
 }
