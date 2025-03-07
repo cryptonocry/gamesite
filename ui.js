ddc8e6e084543a7d430e8da334d06b22b5f3abd3
cryptonocry <132585712+cryptonocry@users.noreply.github.com>
2025-03-07 20:25:38 +0100

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,35 +1,16 @@
 import { fetchAllParticipantsFromXano } from "./api.js";
 
-function maskWallet(wallet) {
-  if (wallet.length <= 8) return wallet;
-  return wallet.substring(0, 4) + "****" + wallet.substring(wallet.length - 4);
-}
-
 export async function showRecordsOverlay(recordsTableContainer, recordsContainer, currentPlayer) {
   const records = await fetchAllParticipantsFromXano();
-  let html = "<table><tr><th>Nickname</th><th>BTC Wallet</th><th>Score</th></tr>";
-  let currentPlayerIndex = -1;
-  records.forEach((rec, index) => {
-    const shortWallet = maskWallet(rec.wallet || "");
-    let rowId = "";
-    if (currentPlayer && rec.nickname === currentPlayer.nickname && rec.wallet === currentPlayer.wallet) {
-      rowId = " id='currentPlayerRow'";
-      currentPlayerIndex = index;
-    }
-    html += `<tr${rowId}>
-               <td>${rec.nickname}</td>
-               <td>${shortWallet}</td>
+  // Отображаем только кошелёк и счёт
+  let html = "<table><tr><th>Wallet</th><th>Score</th></tr>";
+  records.forEach((rec) => {
+    html += `<tr>
+               <td>${rec.wallet}</td>
                <td>${rec.score}</td>
              </tr>`;
   });
   html += "</table>";
   recordsTableContainer.innerHTML = html;
   recordsContainer.style.display = "block";
-
-  setTimeout(() => {
-    const row = document.getElementById("currentPlayerRow");
-    if (row) {
-      row.scrollIntoView({ behavior: "smooth", block: "center" });
-    }
-  }, 100);
 }
