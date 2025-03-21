f4572081b00ac513a61e46871f5e060b98ea27c5
cryptonocry <132585712+cryptonocry@users.noreply.github.com>
2025-03-21 01:07:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -105,12 +105,12 @@ btnRecords.addEventListener("click", () => {
 });
 
 btnBuy.addEventListener("click", () => {
-  window.open("https://odin.fun/", "_blank");
+  window.open("https://odin.fun/token/2b11", "_blank");
 });
 
 const btnTwitter = document.getElementById("btnTwitter");
 btnTwitter.addEventListener("click", () => {
-  window.open("https://odin.fun", "_blank");
+  window.open("https://odin.fun/token/2b11", "_blank");
 });
 
 // ---------- LOGIN OVERLAY ----------
